5009440f53af882721714adc40503dbd0aeec074
fredwu <wufei1988@163.com>
2014-12-23 11:59:58 +0800

    test for strlen() and strstr()

--- editingh.c
+++ editingh.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
+  char str[] = "adfj1213fredwuafjskl";
+  printf("%s\n",str);
+  printf("%d\n",strlen(str));
+  printf("%s\n",strstr(fredwu));
   printf("helloworld\n");
   return 0;
 }
